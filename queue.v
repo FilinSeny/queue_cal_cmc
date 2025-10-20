73c361b8b948a7e9d2acdf920ad307509d213523
FilinSeny <arsen.domoded@gmail.com>
2025-10-20 18:18:13 +0300

    Fixed sintaxis

--- queue.v
+++ queue.v
@@ -6,35 +6,42 @@ module queue(
     input wire rst,
     output reg[15:0] top_conc)
 
-    reg [7:0] arr[4:0];
+    reg [7:0] arr[0:4];
+    integer i;
 
     always @(posedge clk) begin
         if (rst) begin
-            integer i;
+            
             for (i = 0; i < 5; i = i + 1)
                 arr[i] = 8'b0;
+            top_conc = 16'b0;
             ///cicle of reset
         end
         else begin
+            if (pos_back < 5) begin  // проверка границ
+                arr[pos_back] <= back;
+            end
+
+
             case(extra_out)
                 1'b0: begin
                     top_conc = {arr[0], 8'b0};
                     ///cicle of 1 cell shift
-                    integer i;
+                    
                     for (i = 1; i < 5; i = i + 1)
-                        arr[i - 1] = arr[i];
+                        arr[i - 1] <= arr[i];
                     arr[4] = 8'b0; 
                 end;
                 1'b1: begin
                     top_conc = {arr[0], arr[1]};
                     ///cicle of 2 cell shift
-                    integer i;
+                    
                     for (i = 2; i < 5; i = i + 1)
-                        arr[i - 2] = arr[i];
+                        arr[i - 2] <= arr[i];
                     arr[4] = 8'b0;
                     arr[3] = 8'b0;
                 end;
-            assign arr[pos_back] = back;
+            endcase;
         end
 
         
